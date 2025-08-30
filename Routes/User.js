bd4d6975627c732102d8156008ba8e6883e0f5b7
therasak <abdulrasak4567@gmail.com>
2025-08-30 05:56:40 +0530

    booking

--- Routes/User.js
+++ Routes/User.js
@@ -2,16 +2,22 @@ const express = require('express');
 const router = express.Router();
 const {UserManage} = require('../Controllers/UserControl')
 const {upload} = require('../config/upload')
-const {AuthenticateUser, GetUserData,userLoged,userLogOut,userUpdate} = require('../Controllers/UserControl')
+const {AuthenticateUser, GetUserData, userLoged, userLogOut, userUpdate} = require('../Controllers/UserControl')
 const {AuthMiddelWare} = require('../middleware/verifyUser');
 const {verify} = require('jsonwebtoken');
 
 router.post('/addUser', upload.single("profilePic"), UserManage)
 router.post('/login', AuthenticateUser)
+
+
+
 router.post('/getUser', AuthMiddelWare, GetUserData)
-router.post('/IsLogin',AuthMiddelWare,userLoged)
-router.post('/Logout',userLogOut)
-router.patch('/UpdateUser',AuthMiddelWare,userUpdate)
+
+
+
+router.post('/IsLogin', AuthMiddelWare, userLoged)
+router.post('/Logout', userLogOut)
+router.patch('/UpdateUser', AuthMiddelWare, userUpdate)
 
 
 module.exports = router; 
